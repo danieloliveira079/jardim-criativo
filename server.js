84f697e03271ac10da8ec106218725cefe1d202e
Daniel Oliveira Filho <daniel.oliveira079@gmail.com>
2015-09-04 09:58:25 -0300

    node added

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.use("/", express.static(__dirname));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+var port = process.env.PORT || 8080;
+
+var server = app.listen(port, function () {
+
+  var host = server.address().address;
+
+  console.log('Listening at http://%s:%s', host, port);
+});
