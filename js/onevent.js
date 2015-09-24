a9be42565ec23f6afc711ddf74882c39c7272606
Daniel Oliveira Filho <daniel.oliveira079@gmail.com>
2015-09-24 09:44:11 -0300

    updates

--- js/onevent.js
+++ js/onevent.js
@@ -142,7 +142,7 @@ $(document).ready(function() {
 	$('#schedule .nav-tabs li span.arrow').hide();
 
 // Counter
-	var endDate = "September 23, 2015 19:30:00";
+	var endDate = "September 30, 2015 19:30:00";
         $('.countdown.styled').countdown({
           date: endDate,
           render: function(data) {
